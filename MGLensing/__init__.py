fix 3x2pt covariance

MGLensing/__init__.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 import MGrowth as mg
 from scipy import interpolate as itp
 from scipy.linalg import cholesky, solve_triangular
+import matplotlib.pyplot as plt
 
 NL_MODEL_HMCODE = 0
 NL_MODEL_BACCO = 1
@@ -125,7 +126,27 @@ class DataClass():
             }
             self.data_vector = compute_vector[Survey.observable](self.params_data_dic)
             self.data_covariance = compute_covariance[Survey.observable](self.params_data_dic)
-            self.cholesky_transform = cholesky(self.data_covariance, lower=True)
+            plot_cov = False
+            if plot_cov:
+                cmap = 'seismic'
+                cov = self.data_covariance
+                ndata = cov.shape[0]
+                pp_norm = np.zeros((ndata,ndata))
+                for i in range(ndata):
+                    for j in range(ndata):
+                        pp_norm[i][j] = cov[i][j]/np.sqrt(cov[i][i]*cov[j][j])      
+                fig = plt.figure()
+                ax = fig.add_subplot(1, 1, 1)
+                im3 = ax.imshow(pp_norm, cmap=cmap, vmin=-1, vmax=1)
+                fig.colorbar(im3, orientation='vertical')
+                plt.show()
+            try:
+                self.cholesky_transform = cholesky(self.data_covariance, lower=True)
+                print('managed to cholesky')
+                print('######CHOLESKY!!!!########')
+            except:                
+                self.inv_data_covariance = np.linalg.inv(self.data_covariance)   
+                print('######INVERSE COVARIANCE!!!!########') 
 
             
          
@@ -386,7 +407,7 @@ class MGL():
             - err_cl_ll: Error bars for the lensing-lensing component.
             - err_cl_gg: Error bars for the galaxy-galaxy component.
             - err_cl_lg: Error bars for the lensing-galaxy component.
-            - err_cl_gl: Error bars for the galaxy-lensing component.
+
         
         Notes:
         -----
@@ -394,24 +415,26 @@ class MGL():
         by taking the square root of the diagonal elements of the covariance matrix. The error bars are
         then split into different components based on the survey configuration.
         """
-        mask_data_vector_3x2pt = np.ones(int((self.Survey.nell_gc+2*self.Survey.nell_xc+self.Survey.nell_wl)*self.Survey.nbin_flat), dtype=bool)
+        mask_data_vector_3x2pt = np.ones(int(2*self.Survey.lbin*self.Survey.nbin_flat+self.Survey.lbin*self.Survey.nbin**2), dtype=bool)
         self.Data.DataModel.Survey.mask_cov_3x2pt = np.ix_(mask_data_vector_3x2pt, mask_data_vector_3x2pt)
         cov3x2pt = self.Data.DataModel.compute_covariance_3x2pt(params)
         errorbars = np.sqrt(np.diag(cov3x2pt))
-        start_lg = self.Survey.nell_wl*self.Survey.nbin_flat
-        start_gl = (self.Survey.nell_wl+self.Survey.nell_xc)*self.Survey.nbin_flat
-        start_gc = (self.Survey.nell_wl+self.Survey.nell_xc+self.Survey.nell_xc)*self.Survey.nbin_flat
+        start_lg = self.Survey.lbin*self.Survey.nbin_flat 
+        start_gc = start_lg+self.Survey.lbin*self.Survey.nbin**2
         
         errorbars_ll = errorbars[:start_lg]
-        errorbars_lg = errorbars[start_lg:start_gl]
-        errorbars_gl = errorbars[start_gl:start_gc]
+        errorbars_lg = errorbars[start_lg:start_gc]
         errorbars_gg = errorbars[start_gc:]
         #return errorbars_ll, errorbars_gg, errorbars_lg, errorbars_gl
         err_cl_ll = _errorbars_for_plots(self.Survey.nell_wl, self.Survey.nbin, errorbars_ll)
         err_cl_gg = _errorbars_for_plots(self.Survey.nell_gc, self.Survey.nbin, errorbars_gg)
-        err_cl_lg = _errorbars_for_plots(self.Survey.nell_xc, self.Survey.nbin, errorbars_lg)
-        err_cl_gl = _errorbars_for_plots(self.Survey.nell_xc, self.Survey.nbin, errorbars_gl)
-        return  err_cl_ll, err_cl_gg, err_cl_lg, err_cl_gl    
+        err_cl_lg = np.zeros((self.Survey.nell_xc, self.Survey.nbin, self.Survey.nbin))
+        idx_start = 0
+        for bin1 in range(self.Survey.nbin):
+            for bin2 in range(self.Survey.nbin):
+                err_cl_lg[:, bin1, bin2] = errorbars_lg[idx_start*self.Survey.nell_xc:(idx_start+1)*self.Survey.nell_xc]
+                idx_start += 1
+        return  err_cl_ll, err_cl_gg, err_cl_lg    
 
     
     def get_sigma8_from_a_s_from_chain(self, params, nl_model):

MGLensing/likelihood.py
@@ -125,8 +125,11 @@ class MGLike:
             chi2 = 0.
             model_data_vector = self.compute_data_vector(param_dic_all) 
             difference_vector = self.Data.data_vector - model_data_vector
-            yt = solve_triangular(self.Data.cholesky_transform, difference_vector, lower=True)
-            chi2 = yt.dot(yt)
+            try:
+                yt = solve_triangular(self.Data.cholesky_transform, difference_vector, lower=True)
+                chi2 = yt.dot(yt)
+            except:
+                chi2 = np.einsum('i, ij, j->', difference_vector, self.Data.inv_data_covariance, difference_vector)    
             return -0.5*chi2    
         else:
             return -np.inf      

MGLensing/specs.py
@@ -7,6 +7,7 @@ import sacc
 import yaml
 import warnings
 import os
+DEG2_IN_SPHERE = 4 * np.pi * (180 / np.pi)**2
 
 dirname = os.path.split(__file__)[0]
 print(dirname)
@@ -201,31 +202,10 @@ def setup_const_lmax(obj, likelihood):
         obj.ells_gc = np.array(range(obj.lmin, int(obj.lmax_gc_vals)+1)) # all integer ells
         obj.ell_jump =  int(obj.lmax_gc_vals) - obj.lmin + 1
     else:
-        idx_lmax_wl = (obj.ell_bin_edges<=obj.lmax_wl_vals)
-        idx_lmax_gc = (obj.ell_bin_edges<=obj.lmax_gc_vals)
-        # ell bineed cut at lmax
-        # we set ells for the cross-correlation to be equal to photo-gc
-        # can be changed later
-        obj.l_wl = obj.l_wl_bin_centers[idx_lmax_wl[1:]] 
-        obj.l_xc = obj.l_gc = obj.l_wl_bin_centers[idx_lmax_gc[1:]] 
-        obj.nell_wl = len(obj.l_wl)
-        obj.nell_xc = obj.nell_gc = len(obj.l_gc)
-        # this is not used, computed for completness
-        # all integer ells
-        obj.ells_wl = np.array(range(obj.lmin, int(obj.lmax_wl_vals)+1)) 
-        obj.ells_gc = np.array(range(obj.lmin, int(obj.lmax_gc_vals)+1)) 
-        obj.ell_jump =  int(obj.lmax_gc_vals) - obj.lmin + 1
-        # compute masks (basically does nothing as the model is already computed for l<lmax)
-        obj.mask_data_vector_wl = np.ones((obj.nell_wl*nbin_flat), dtype=bool)
-        obj.mask_cov_wl = np.ix_(obj.mask_data_vector_wl, obj.mask_data_vector_wl)
-        obj.mask_data_vector_gc = np.ones((obj.nell_gc*nbin_flat), dtype=bool)
-        obj.mask_cov_gc = np.ix_(obj.mask_data_vector_gc, obj.mask_data_vector_gc)
-        obj.mask_data_vector_3x2pt = np.ones(int((obj.nell_gc+2*obj.nell_xc+obj.nell_wl)*nbin_flat), dtype=bool)
-        obj.mask_cov_3x2pt = np.ix_(obj.mask_data_vector_3x2pt, obj.mask_data_vector_3x2pt)
-        # delta ells for covariance computation
-        obj.d_ell_bin_cut_wl = obj.d_ell_bin[idx_lmax_wl[1:]] 
-        obj.d_ell_bin_cut_xc = obj.d_ell_bin[idx_lmax_gc[1:]] 
-        obj.d_ell_bin_cut_gc = obj.d_ell_bin[idx_lmax_gc[1:]] 
+        obj.lmax_wl_vals = np.full(obj.nbin_flat, obj.lmax_wl_vals)
+        obj.lmax_gc_vals = np.full(obj.nbin_flat, obj.lmax_gc_vals)
+        setup_lmax(obj)
+
     obj.ells_wl_max = max_ells_for_plots(obj.nbin, np.full(nbin_flat, obj.lmax_wl_vals))
     obj.ells_gc_max = max_ells_for_plots(obj.nbin, np.full(nbin_flat, obj.lmax_gc_vals))    
 
@@ -257,8 +237,6 @@ def setup_lmax(obj):
     # for binned ansatz:
     # we compute model/data/covariance at all available ell-bins
     # and apply mask afterwards;
-    # alternatively, we can compute the model already at different lmax per probe
-    # the current implementation in the Theory class allows it
     obj.l_wl = obj.l_xc = obj.l_gc = obj.l_wl_bin_centers
     obj.nell_wl = obj.nell_xc = obj.nell_gc = len(obj.l_wl)
     # delta ells for covariance computation
@@ -269,9 +247,15 @@ def setup_lmax(obj):
     mask_ells_wl = mask_ells_wl[:, 1:]
     mask_ells_gc = np.array([(obj.ell_bin_edges<= lmax_gc_i) for lmax_gc_i in obj.lmax_gc_vals])
     mask_ells_gc = mask_ells_gc[:, 1:]
+    # later add an option to define xc cuts independently
+    lmax_xc_vals_2d = max_ells_for_plots(obj.nbin, obj.lmax_gc_vals)
+    obj.lmax_xc_vals = np.concatenate(lmax_xc_vals_2d)
+    mask_ells_xc = np.array([(obj.ell_bin_edges<= lmax_xc_i) for lmax_xc_i in obj.lmax_xc_vals])
+    mask_ells_xc = mask_ells_xc[:, 1:]
     obj.mask_data_vector_wl = np.concatenate((mask_ells_wl))
     obj.mask_data_vector_gc = np.concatenate((mask_ells_gc))
-    obj.mask_data_vector_3x2pt = np.concatenate((obj.mask_data_vector_wl, obj.mask_data_vector_gc, obj.mask_data_vector_gc, obj.mask_data_vector_gc))
+    obj.mask_data_vector_xc = np.concatenate((mask_ells_xc))
+    obj.mask_data_vector_3x2pt = np.concatenate((obj.mask_data_vector_wl, obj.mask_data_vector_xc, obj.mask_data_vector_gc))
     obj.mask_cov_wl = np.ix_(obj.mask_data_vector_wl, obj.mask_data_vector_wl)
     obj.mask_cov_gc = np.ix_(obj.mask_data_vector_gc, obj.mask_data_vector_gc)
     obj.mask_cov_3x2pt = np.ix_(obj.mask_data_vector_3x2pt, obj.mask_data_vector_3x2pt)
@@ -651,13 +635,13 @@ class EuclidSetUp:
         self.gal_per_sqarcmn = 30.0
         self.rms_shear = 0.30
         self.fsky = 0.375 #15k deg^2
-        self.lbin = 100
+        self.lbin = 20 #100
         self.lmin = 10
         self.lmax = 5000
         if survey_info == 'Euclid_5bins':
             self.nbin = 5 
             self.z_bin_edge = np.array([self.zmin, 0.560, 0.789, 1.019, 1.324, self.zmax])  
-            self.fsky = 0.375 #15k deg^2
+            self.fsky = 0.375 #15k deg^2 #sky coverage 14_700  in in deg^2, fskay=sky coverage/DEG2_IN_SPHERE
 
         elif survey_info == 'Euclid_Y1':
             self.nbin = 5 
@@ -671,7 +655,7 @@ class EuclidSetUp:
         elif survey_info == 'Euclid_10bins':    
             self.nbin = 10
             self.z_bin_edge = np.array([self.zmin, 0.418, 0.560, 0.678, 0.789, 0.900, 1.019, 1.155, 1.324, 1.576, 2.5])
-            self.fsky = 0.4
+            self.fsky = 14_700/DEG2_IN_SPHERE #0.4 
         self.z_bin_center = np.array([(self.z_bin_edge[i]+self.z_bin_edge[i+1])/2 for i in range(self.nbin)])
         self.z_bin_center_s = self.z_bin_center_l = self.z_bin_center
         # z values for integration (!= from z bins)